f394d9d6dfcd5e18dc6e6c80cbf750a15c2f56e2
nushibo <nushiboninja@hotmail.com>
2017-07-06 03:52:07 -0400

    Update

---
+++ public/js/app.js
@@ -0,0 +1,139 @@
+// app.js
+
+$('document').ready(function() {
+
+  var webAuth = new auth0.WebAuth({
+    domain: AUTH0_DOMAIN,
+    clientID: AUTH0_CLIENT_ID,
+    redirectUri: AUTH0_CALLBACK_URL,
+    audience: 'https://' + AUTH0_DOMAIN + '/userinfo',
+    responseType: 'token id_token',
+    scope: 'openid'
+  });
+
+  var loginBtn = $('#btn-login');
+
+  loginBtn.click(function(e) {
+    e.preventDefault();
+    webAuth.authorize();
+  });
+
+});
+
+
+angular.module("contactsApp", ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "list.html",
+                controller: "ListController",
+                resolve: {
+                    contacts: function(Contacts) {
+                        return Contacts.getContacts();
+                    }
+                }
+            })
+            .when("/new/contact", {
+                controller: "NewContactController",
+                templateUrl: "contact-form.html"
+            })
+            .when("/contact/:contactId", {
+                controller: "EditContactController",
+                templateUrl: "contact.html"
+            })
+            .otherwise({
+                redirectTo: "/"
+            })
+    })
+    .service("Contacts", function($http) {
+        this.getContacts = function() {
+            return $http.get("/contacts").
+                then(function(response) {
+                    return response;
+                }, function(response) {
+                    alert("Error finding contacts.");
+                });
+        }
+        this.createContact = function(contact) {
+            return $http.post("/contacts", contact).
+                then(function(response) {
+                    return response;
+                }, function(response) {
+                    alert("Error creating contact.");
+                });
+        }
+        this.getContact = function(contactId) {
+            var url = "/contacts/" + contactId;
+            return $http.get(url).
+                then(function(response) {
+                    return response;
+                }, function(response) {
+                    alert("Error finding this contact.");
+                });
+        }
+        this.editContact = function(contact) {
+            var url = "/contacts/" + contact._id;
+            console.log(contact._id);
+            return $http.put(url, contact).
+                then(function(response) {
+                    return response;
+                }, function(response) {
+                    alert("Error editing this contact.");
+                    console.log(response);
+                });
+        }
+        this.deleteContact = function(contactId) {
+            var url = "/contacts/" + contactId;
+            return $http.delete(url).
+                then(function(response) {
+                    return response;
+                }, function(response) {
+                    alert("Error deleting this contact.");
+                    console.log(response);
+                });
+        }
+    })
+    .controller("ListController", function(contacts, $scope) {
+        $scope.contacts = contacts.data;
+    })
+    .controller("NewContactController", function($scope, $location, Contacts) {
+        $scope.back = function() {
+            $location.path("#/");
+        }
+
+        $scope.saveContact = function(contact) {
+            Contacts.createContact(contact).then(function(doc) {
+                var contactUrl = "/contact/" + doc.data._id;
+                $location.path(contactUrl);
+            }, function(response) {
+                alert(response);
+            });
+        }
+    })
+    .controller("EditContactController", function($scope, $routeParams, Contacts) {
+        Contacts.getContact($routeParams.contactId).then(function(doc) {
+            $scope.contact = doc.data;
+        }, function(response) {
+            alert(response);
+        });
+
+        $scope.toggleEdit = function() {
+            $scope.editMode = true;
+            $scope.contactFormUrl = "contact-form.html";
+        }
+
+        $scope.back = function() {
+            $scope.editMode = false;
+            $scope.contactFormUrl = "";
+        }
+
+        $scope.saveContact = function(contact) {
+            Contacts.editContact(contact);
+            $scope.editMode = false;
+            $scope.contactFormUrl = "";
+        }
+
+        $scope.deleteContact = function(contactId) {
+            Contacts.deleteContact(contactId);
+        }
+    });
